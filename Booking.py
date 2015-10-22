2208c4be55a1dcec95e711a401dab9e9b3b4660c
dktrotti <dktrotti@ualberta.ca>
2015-10-22 16:05:35 -0600

    Added funtionality to BookingScreen

--- Booking.py
+++ Booking.py
@@ -1,8 +1,5 @@
-import sys
-import Database
-
 def bookingQuery(database, email):
-	query = """SELECT b.tno, t.name, b.dep_date, t.paid_price FROM
+	query = """SELECT t.name, t.paid_price, b.* FROM
 			bookings b, tickets t WHERE
 			t.tno = b.tno AND
 			t.email = '{}'""".format(email)
