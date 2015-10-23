95f81850a66c860b0315eeb722d4862f31a0abfc
dktrotti <dktrotti@ualberta.ca>
2015-10-22 19:36:45 -0600

    Added support for places with 24 hours in a day

--- Agent.py
+++ Agent.py
@@ -1,6 +1,6 @@
 def recordDeparture(database, flightno, dep_date, time):
 	query = """UPDATE sch_flights
-			SET act_dep_time = TO_DATE('{0}', 'HH:MI:SS')
+			SET act_dep_time = TO_DATE('{0}', 'HH24:MI:SS')
 			WHERE flightno = '{1}' AND dep_date = TO_DATE('{2}', 'YYYY-MM-DD')""".format(time, flightno, dep_date)
 			
 	database.cursor.execute(query)
@@ -8,7 +8,7 @@ def recordDeparture(database, flightno, dep_date, time):
 
 def recordArrival(database, flightno, time):
 	query = """UPDATE sch_flights
-			SET act_arr_time = TO_DATE('{0}', 'HH:MI:SS')
+			SET act_arr_time = TO_DATE('{0}', 'HH24:MI:SS')
 			WHERE flightno = '{1}' AND dep_date = TO_DATE('{2}', 'YYYY-MM-DD')""".format(time, flightno, dep_date)
 			
 	database.cursor.execute(query)
