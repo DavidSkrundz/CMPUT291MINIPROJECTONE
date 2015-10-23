4ce0042c28d3578a60c6877d47c645d9e5235aa6
Ryan Huard <ryan.huard@live.ca>
2015-10-22 19:00:18 -0600

    Returns a single booking (there) also no longer prints back because muh return statements try Yeg-UUK 2015-06-13

--- Booking.py
+++ Booking.py
@@ -3,14 +3,14 @@ def bookingQuery(database, email):
 			bookings b, tickets t WHERE
 			t.tno = b.tno AND
 			t.email = '{}'""".format(email)
-	
+
 	database.cursor.execute(query)
-	
-def addBooking(database, tno, flightno, fare, dep_date, seat)
+
+def addBooking(database, tno, flightno, fare, dep_date, seat):
 	query = """INSERT INTO bookings
 			VALUES({0}, '{1}', '{2}', TO_DATE('{3}'), '{4}')""".format(
 				tno, flightno, fare, dep_date, seat)
-	
+
 	database.cursor.execute(query)
 	database.commit()
 
@@ -18,7 +18,6 @@ def cancelBooking(database, tno):
 	query = """DELETE FROM
 			bookings b WHERE
 			b.tno = {}""".format(tno)
-	
+
 	database.cursor.execute(query)
 	database.commit()
-			
