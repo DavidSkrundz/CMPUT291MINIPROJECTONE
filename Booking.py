871d87e4f316f05fa86f851c97ba6462ad4f0a16
dktrotti <dktrotti@ualberta.ca>
2015-10-22 18:37:25 -0600

    Added addBooking

--- Booking.py
+++ Booking.py
@@ -5,11 +5,19 @@ def bookingQuery(database, email):
 			t.email = '{}'""".format(email)
 	
 	database.cursor.execute(query)
+	
+def addBooking(database, tno, flightno, fare, dep_date, seat)
+	query = """INSERT INTO bookings
+			VALUES({0}, '{1}', '{2}', TO_DATE('{3}'), '{4}')""".format(
+				tno, flightno, fare, dep_date, seat)
+	
+	database.cursor.execute(query)
+	database.commit()
 
 def cancelBooking(database, tno):
 	query = """DELETE FROM
 			bookings b WHERE
-			b.tno = '{}'""".format(tno)
+			b.tno = {}""".format(tno)
 	
 	database.cursor.execute(query)
 	database.commit()
