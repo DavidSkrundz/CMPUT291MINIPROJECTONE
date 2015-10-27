66eacd9cce9eed8dbe55f722bd5eb8458fb6c696
David Skrundz <david.skrundz@gmail.com>
2015-10-27 15:13:53 -0600

    some fixes

--- BookingScreen.py
+++ BookingScreen.py
@@ -8,6 +8,7 @@ def bookingScreen(database, email):
 		Util.clear()
 		Booking.bookingQuery(database, email)
 		bookings = database.cursor.fetchall()
+		print("0 to go back")
 		Util.print_table(["Row #", "Ticket no.", "Name", "Date", "Price"],
 							[6, 10, 20, 20, 10],
 							bookings,
@@ -19,8 +20,10 @@ def bookingScreen(database, email):
 			print("No bookings.")
 			input("")
 			return
-		selection = input("Select booking:")
-		if selection > len(bookings):
+		selection = int(input("Select booking:"))
+		if selection == 0:
+			return
+		elif selection > len(bookings):
 			print("Invalid selection (enter to continue)")
 			input("")
 			continue
@@ -36,6 +39,6 @@ def bookingScreen(database, email):
 			print("")
 			print("1. Delete")
 			print("2. Back")
-			selection = input("")
+			selection = int(input(""))
 			if selection == 1:
 				Booking.cancelBooking(database, bookings[selection][2])
